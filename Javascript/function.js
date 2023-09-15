ef8652cc07c0ad20651f872b3487bc2fe5f44d99
Saketh-Bejadi <bejadisakethreddy168@gmail.com>
2023-09-15 14:25:42 +0530

    Modified

--- Javascript/function.js
+++ Javascript/function.js
@@ -1,1 +1,13 @@
 console.log("Hello World");
+var a=-1;
+var b=20;
+if (a>0){
+    console.log("a is positive")
+}
+if (a<0){
+    console.log("a negative")
+}
+if (a<=0) {
+    console.log("a is negative or zero")
+}
+
