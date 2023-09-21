36dffad1e9ce868042e2ccfa9d138ac1c640a305
Saketh-Bejadi <bejadisakethreddy168@gmail.com>
2023-09-21 19:22:41 +0530

    Assets added

--- Javascript/function.js
+++ Javascript/function.js
@@ -38,3 +38,26 @@ else{
     console.log("Invalid value");
 }
 
+
+// Practice Question
+let size="S";
+if (size=="XL") {
+    console.log("Price:Rs.250");
+}
+else if (size=="L"){
+    console.log("Price:Rs.200");
+}
+else if(size=="M"){
+    console.log("Price:Rs.100");
+}
+else if(size=="S"){
+    console.log("Price:Rs.50");
+}
+else {
+    console.log("Invalid Size");
+}
+
+
+
+
+let string="apple";
