a48cfbea000bf3f8419479d2d00ab4ea98302ff4
Saketh-Bejadi <bejadisakethreddy168@gmail.com>
2023-10-01 23:28:51 +0530

    modified

--- Bootstrap/function.js
+++ Bootstrap/function.js
@@ -6,14 +6,13 @@ const getNewcard=(taskData)=>
     <div class="column col-sm-12 col-md-6 col-lg-4 mt-2 mb-2">
     <div class="card">
       <div class="card-header d-flex gap-2 justify-content-end">
-        <button type="button" class="btn btn-outline-success"><i class="fa-solid fa-pencil"></i></button>
+        <button type="button" class="btn btn-outline-success"><i class="fa-solid fa-check fa-lg"></i></button>
         <button type="button" class="btn btn-outline-danger"><i class="fa-solid fa-trash-can"></i></button>
       </div>
       <div class="card-body">
         <img src=${taskData.imgURL} alt="...." class="card-img-top">
         <h5 class="card-title mt-3 fw-bold text-primary">${taskData.taskTitle}</h5>
         <p class="card-text">${taskData.taskDescription}</p>
-        <a href="#" class="btn btn-primary">${taskData.taskType}</a>
       </div>
     </div>
   </div>
@@ -36,11 +35,10 @@ const saveChanges=() =>{
         id:`${Date.now()}`,
         imgURL:document.getElementById("ImgURL").value,
         taskTitle:document.getElementById("TaskTitle").value,
-        taskType:document.getElementById("TaskType").value,
         taskDescription:document.getElementById("TaskDescription").value,
     }
     // console.log(taskData);
-    if (taskData.id,taskData.imgURL,taskData.taskTitle,taskData.taskType,taskData.taskDescription) {
+    if (taskData.id,taskData.imgURL,taskData.taskTitle,taskData.taskDescription) {
   //       const newCARD=`
   //   <div class="column col-sm-12 col-md-6 col-lg-4 mt-2 mb-2">
   //   <div class="card">
