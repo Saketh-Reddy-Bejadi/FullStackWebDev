34f0c64fe4fc291ca12fa076cb5c0af4e9b81fbd
Saketh-Bejadi <bejadisakethreddy168@gmail.com>
2023-10-05 00:31:42 +0530

    http

--- Node/index.js
+++ Node/index.js
@@ -6,6 +6,14 @@
 // });
 
 
-const OperatingSystem=require("os");
-console.log(OperatingSystem.platform());  //for name of os
-console.log(OperatingSystem.arch());      //for type of os like 64 or 32-bit
+// const OperatingSystem=require("os");
+// console.log(OperatingSystem.platform());  //for name of os
+// console.log(OperatingSystem.arch());      //for type of os like 64 or 32-bit
+
+
+
+const http=require("http");
+http.createServer((request,response)=>{
+    console.log(request.headers);
+    response.end("<h1>NodeJS</h1>");
+})  .listen(1211);
