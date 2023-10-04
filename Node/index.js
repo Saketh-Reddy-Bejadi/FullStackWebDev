7849ca66d5be749ccb6c4dd216f28d0bcc7af5c4
Saketh-Bejadi <bejadisakethreddy168@gmail.com>
2023-10-05 00:10:46 +0530

    OS

--- Node/index.js
+++ Node/index.js
@@ -1,6 +1,11 @@
-const FileSystem=require("fs");
-FileSystem.rmdir("skill",function(err,reult){
-    if(err) {
-        console.log(err);
-    }
-});
+// const FileSystem=require("fs");
+// FileSystem.rmdir("skill",function(err,reult){   //mkdir,rmdir,writeFile
+//     if(err) {
+//         console.log(err);
+//     }
+// });
+
+
+const OperatingSystem=require("os");
+console.log(OperatingSystem.platform());  //for name of os
+console.log(OperatingSystem.arch());      //for type of os like 64 or 32-bit
