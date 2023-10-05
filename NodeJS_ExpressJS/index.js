7c3f0c0b004bb5939b1decb933657487e44a5567
Saketh-Bejadi <bejadisakethreddy168@gmail.com>
2023-10-05 23:54:22 +0530

    Completed with basics of ExpressJs

--- NodeJS_ExpressJS/index.js
+++ NodeJS_ExpressJS/index.js
@@ -14,7 +14,6 @@ console.log(OperatingSystem.platform());  //for name of os
 console.log(OperatingSystem.arch());      //for type of os like 64 or 32-bit
 
 
-
 const http=require("http");
 http.createServer((request,response)=>{
     console.log(request.headers);
@@ -31,3 +30,57 @@ http.createServer((request,response)=>{
         response.end("<h1>About</h1>")
     }
 }).listen(3000);*/
+
+
+
+// ExpressJS
+// const express=require("express");
+
+// const noddy=express();
+
+// noddy.use(express.json());
+
+// noddy.get("/",(request,response)=>{
+//     return response.json({data:"Hello World!"});
+// });
+
+// noddy.listen(3000,()=>{
+//     console.log("Server is running at port 3000");
+// });
+
+
+
+const express=require("express");
+const node=express();
+// node.use(express.json())
+
+node.get("/",(req,res)=>{
+    res.end("<h1>Hello world!</h1>");
+});
+
+node.get("/about",(req,res)=>{
+    res.end("<h1>About Section</h1>");
+});
+
+node.get("/b/:id",(req,res) =>{
+
+    const students=[
+        {id:1,name:"student1"},
+        {id:2,name:"student2"},
+        {id:3,name:"student3"},
+        {id:4,name:"student4"},
+        {id:5,name:"student5"},
+        {id:6,name:"student6"}
+    ]
+
+    const studentID=req.params.id;
+    const getStudent=students.filter((student)=> student.id==studentID);
+        return res.json({data:getStudent});
+
+})
+
+node.listen(3200,()=>{
+    console.log("Server is running at port 3200");
+})
+
+
