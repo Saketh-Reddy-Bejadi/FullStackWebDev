713157bb6dfa97eaf475169b9e9312538c201436
Saketh-Bejadi <bejadisakethreddy168@gmail.com>
2023-10-05 22:03:53 +0530

    Started Express

---
+++ NodeJS_ExpressJS/index.js
@@ -0,0 +1,33 @@
+
+// Nodejs
+
+/*const FileSystem=require("fs");
+FileSystem.rmdir("skill",function(err,reult){   //mkdir,rmdir,writeFile
+    if(err) {
+        console.log(err);
+    }
+});
+
+
+const OperatingSystem=require("os");
+console.log(OperatingSystem.platform());  //for name of os
+console.log(OperatingSystem.arch());      //for type of os like 64 or 32-bit
+
+
+
+const http=require("http");
+http.createServer((request,response)=>{
+    console.log(request.headers);
+    response.end("<h1>NodeJS</h1>");
+})  .listen(1211);
+
+
+const http=require("http");
+http.createServer((request,response)=>{
+    if (request.url==="/") {
+        response.end("<h1>Main</h1>");
+    }
+    else if(request.url==="/about"){
+        response.end("<h1>About</h1>")
+    }
+}).listen(3000);*/
